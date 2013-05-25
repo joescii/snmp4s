2ded2283feb93e4f16643a2b73ae846e4c9f4bca
Joe Barnes <barnesjd@gmail.com>
2013-05-25 17:58:40 -0500

    Code generation added for some basic OID types

--- gen/src/main/scala/org/snmp4s/gen/Gen.scala
+++ gen/src/main/scala/org/snmp4s/gen/Gen.scala
@@ -6,9 +6,29 @@
 package org.snmp4s.gen
 
 import net.percederberg.mibble._
+import net.percederberg.mibble.snmp._
 import java.io.File
+import scala.collection.JavaConversions._
+
+protected object Util {
+  def name2oid(mib:Mib) = {
+    val syms = mib.getAllSymbols()
+    (for {
+        sym <- syms
+        if (sym.isInstanceOf[MibValueSymbol])
+      } yield {
+        val s = sym.asInstanceOf[MibValueSymbol]
+        s.getName -> s
+      }).toMap
+  }
+}
 
 class Gen {
+  def load(name:String):Mib = {
+    val loader = new MibLoader
+    loader load name
+  }
+  
   def load(file:File):Seq[Mib] = {
     val loader = new MibLoader
     loader.addDir(file)
@@ -27,5 +47,34 @@ class Gen {
     }).flatten.toSeq
   }
   
+  def code(oid:MibValueSymbol):String = {
+    val name = oid.getName
+    val objName = name.substring(0, 1).toUpperCase() + name.substring(1)
+    if(oid.getType.isInstanceOf[SnmpObjectType]) {
+      val snmp = oid.getType.asInstanceOf[SnmpObjectType]
+      val access = accessMap.get(snmp.getAccess()).get
+      val octets = oid.getValue.toString.replace(".", ",")
+      val syntax = syntaxMap.get(snmp.getSyntax().getName()).get
+            
+      val code = s"""case object $objName extends AccessibleObject[$access, $syntax](Seq($octets), "$name")"""
+      if(oid.isScalar) code + s" with Scalar[$access, $syntax]"
+      else code
+    } else {
+      ""
+    }
+  }
   
+  private val accessMap = Map(
+    SnmpAccess.READ_WRITE -> "ReadWrite",
+    SnmpAccess.NOT_ACCESSIBLE -> "NotAccessible",
+    SnmpAccess.ACCESSIBLE_FOR_NOTIFY -> "AccessibleForNotify",
+    SnmpAccess.READ_CREATE -> "ReadCreate",
+    SnmpAccess.READ_ONLY -> "ReadOnly",
+    SnmpAccess.WRITE_ONLY -> "WriteOnly"
+  )
+  
+  private val syntaxMap = Map(
+    "INTEGER" -> "Int",
+    "OCTET STRING" -> "String"
+  )
 }
--- gen/src/test/scala/org/snmp4s/gen/GenSuite.scala
+++ gen/src/test/scala/org/snmp4s/gen/GenSuite.scala
@@ -8,19 +8,54 @@ package org.snmp4s.gen
 import org.scalatest.{WordSpec}
 import org.scalatest.matchers.{ShouldMatchers}
 import java.io.File
+import scala.collection.JavaConversions._
+import net.percederberg.mibble._
 
 class GenSuite extends WordSpec with ShouldMatchers {
   "A Generator" should {
     "Load agent mib" in {
       val g = new Gen
       val ms = g.load(new File("gen/src/test/mibs"))
+      val name2mib = ms.map { m => m.getName -> m }.toMap
 
       ms.map { _ getName }.toSet should equal (Set(
         "AGENTPP-GLOBAL-REG",
         "AGENTPP-SIMULATION-MIB"
       ))
+      
+      val sim = name2mib.get("AGENTPP-SIMULATION-MIB")
+      sim.isDefined should equal(true)
+      val name2oid = Util.name2oid(sim.get)
+      
+      name2oid.keySet should equal (Set(
+        "agentppSimMIB",
+        "agentppSim",
+        "agentppSimMode",
+        "agentppSimDeleteRow",
+        "agentppSimDeleteTableContents",
+        "agentppSimBasicGroup",
+        "agentppSimCompliance"
+      ))
+      
+      val simMode = name2oid.get("agentppSimMode").get
+      val code = g.code(simMode)
+      
+      code should equal ("""case object AgentppSimMode extends AccessibleObject[ReadWrite, Int](Seq(1,3,6,1,4,1,4976,2,1,1), "agentppSimMode") with Scalar[ReadWrite, Int]""")
     }
     
-    
+    "generate code for IF-MIB" in {
+      val g = new Gen
+      def ifMib = g.load("IF-MIB")
+      val name2oid = Util.name2oid(ifMib)
+      val ifMtu = name2oid.get("ifMtu").get
+      val ifDescr = name2oid.get("ifDescr").get
+      val ifSpeed = name2oid.get("ifSpeed").get
+      val ifLastChange = name2oid.get("ifLastChange").get
+      
+      g.code(ifDescr) should equal ("""case object IfDescr extends AccessibleObject[ReadOnly, String](Seq(1,3,6,1,2,1,2,2,1,2), "ifDescr")""")
+      g.code(ifMtu) should equal ("""case object IfMtu extends AccessibleObject[ReadOnly, Int](Seq(1,3,6,1,2,1,2,2,1,4), "ifMtu")""")
+      g.code(ifSpeed) should equal ("""case object IfSpeed extends AccessibleObject[ReadOnly, Int](Seq(1,3,6,1,2,1,2,2,1,5), "ifSpeed")""")
+      g.code(ifLastChange) should equal ("""case object IfLastChange extends AccessibleObject[ReadOnly, Int](Seq(1,3,6,1,2,1,2,2,1,9), "ifLastChange")""")
+    }
   }
 }
