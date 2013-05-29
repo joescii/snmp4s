8e8cee73c7e282055630ad796e26d8b423444dd2
Joe Barnes <barnesjd@gmail.com>
2013-05-29 11:55:58 -0500

    Bootstrapped the snmp4s-sbt project

--- project/Build.scala
+++ project/Build.scala
@@ -2,12 +2,15 @@ import sbt._
 import Keys._
 
 object Snmp4s extends Build {
-    lazy val root = Project(id = "snmp4s",
-                            base = file(".")) aggregate(core, gen)
+  lazy val root = Project(id = "snmp4s",
+                          base = file(".")) aggregate(core, gen, sbtp)
 
-    lazy val core = Project(id = "snmp4s-core",
-                           base = file("core"))
+  lazy val core = Project(id = "snmp4s-core",
+                          base = file("core"))
 
-    lazy val gen  = Project(id = "snmp4s-gen",
-                           base = file("gen"))
+  lazy val gen  = Project(id = "snmp4s-gen",
+                          base = file("gen"))
+
+  lazy val sbtp = Project(id = "snmp4s-sbt",
+                          base = file("sbt"))
 }
---
+++ sbt/Snmp4sSbtPlugin.scala
@@ -0,0 +1,18 @@
+import sbt._
+object Snmp4sSbtPlugin extends Plugin
+{
+    // configuration points, like the built in `version`, `libraryDependencies`, or `compile`
+    // by implementing Plugin, these are automatically imported in a user's `build.sbt`
+    val genMibsTask = TaskKey[Unit]("snmp4s-gen-mibs")
+    val builtInMibsSetting = SettingKey[String]("snmp4s-built-in-mibs")
+
+    // a group of settings ready to be added to a Project
+    // to automatically add them, do
+    val snmp4sSettings = Seq(
+        builtInMibsSetting := "IfMib",
+        genMibsTask <<= builtInMibsSetting map { str => println(str) }
+    )
+
+    // alternatively, by overriding `settings`, they could be automatically added to a Project
+    // override val settings = Seq(...)
+}
