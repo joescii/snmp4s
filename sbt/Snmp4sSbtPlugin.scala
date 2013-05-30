11d1f84da81286002448486a414bb490eda0be9f
Joe Barnes <barnesjd@gmail.com>
2013-05-30 11:44:52 -0500

    Updated to begin using the snmp4s-gen library

--- sbt/Snmp4sSbtPlugin.scala
+++ sbt/Snmp4sSbtPlugin.scala
@@ -4,12 +4,16 @@ import org.snmp4s.gen._
 object Snmp4sSbtPlugin extends Plugin
 {
   val genMibsTask = TaskKey[Unit]("snmp4s-gen-mibs")
-  val builtInMibsSetting = SettingKey[String]("snmp4s-built-in-mibs")
+  val builtInMibsSetting = SettingKey[Seq[BuiltIn.Value]]("snmp4s-built-in-mibs")
 
   val snmp4sSettings = Seq(
-    builtInMibsSetting := "IfMib",
-    genMibsTask <<= builtInMibsSetting map { mib => 
-      println("Compiling " + mib)
+    builtInMibsSetting := Seq(BuiltIn.IfMib),
+    genMibsTask <<= builtInMibsSetting map { mibs => 
+      println("Compiling " + mibs)
+      mibs map { mib => 
+        val g = new Gen
+        println(g.code("org.snmp4s.mib", g.load(mib))) 
+      }
     }
   )
 
--- sbt/build.sbt
+++ sbt/build.sbt
@@ -10,7 +10,11 @@ scalaVersion := "2.9.2"
 
 scalacOptions := Seq("-deprecation", "-unchecked")
 
+resolvers += "Mibble" at "http://maven.cloudhopper.com/repos/third-party/"
+
 libraryDependencies ++= Seq(
+  "snmp" % "mibble-parser" % "2.9.2",
+  "snmp" % "mibble-mibs" % "2.9.2",
   "org.snmp4s" %% "snmp4s-gen" % "0.1.0"
 )
 
