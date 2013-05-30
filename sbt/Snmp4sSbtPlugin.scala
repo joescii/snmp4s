6704c190a658517c29ed2e1a2e78e009f40b4b2f
Joe Barnes <barnesjd@gmail.com>
2013-05-30 14:06:47 -0500

    Updated so that IF-MIB is successfully generated on compile

--- sbt/Snmp4sSbtPlugin.scala
+++ sbt/Snmp4sSbtPlugin.scala
@@ -1,20 +1,28 @@
 import sbt._
+import sbt.Keys._
 import org.snmp4s.gen._
 
 object Snmp4sSbtPlugin extends Plugin
 {
-  val genMibsTask = TaskKey[Unit]("snmp4s-gen-mibs")
   val builtInMibsSetting = SettingKey[Seq[BuiltIn.Value]]("snmp4s-built-in-mibs")
 
   val snmp4sSettings = Seq(
     builtInMibsSetting := Seq(BuiltIn.IfMib),
-    genMibsTask <<= builtInMibsSetting map { mibs => 
-      println("Compiling " + mibs)
-      mibs map { mib => 
-        val g = new Gen
-        println(g.code("org.snmp4s.mib", g.load(mib))) 
-      }
+    sourceGenerators in Compile <+= sourceManaged in Compile map { outDir: File =>
+      genMibs(outDir / "mibs")
     }
   )
 
+
+  def genMibs(dst:File): Seq[File] = { 
+    val mibs = Seq(BuiltIn.IfMib) 
+    println("Compiling " + mibs)
+    mibs map { mib => 
+      val file = dst / "org" / "snmp4s" / "mib" / "IfMib.scala"
+      val g = new Gen
+      IO.write(file, g.code("org.snmp4s.mib", g.load(mib)))
+      file 
+    }
+    
+  }
 }
