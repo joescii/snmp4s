0ca2fff3daf543e98bca55ddc2e33fdbc7a72a5f
Joe Barnes <barnesjd@gmail.com>
2013-05-30 21:34:41 -0500

    Added a log message to let the user know how many MIBs are being processed

--- sbt/Snmp4sSbtPlugin.scala
+++ sbt/Snmp4sSbtPlugin.scala
@@ -10,15 +10,18 @@ object Snmp4sSbtPlugin extends Plugin
   val snmp4sSettings = Seq(
     snmp4sBuiltInMibs := Seq(),
     snmp4sMibPackage := "org.snmp4s.mib",
-    sourceGenerators in Compile <+= (sourceManaged in Compile, snmp4sBuiltInMibs, snmp4sMibPackage) map { (outDir: File, mibs: Seq[BuiltIn.Value], pkg: String) =>
-      genMibs(outDir / "mibs", mibs, pkg)
+    sourceGenerators in Compile <+= (sourceManaged in Compile, snmp4sBuiltInMibs, snmp4sMibPackage, streams) map { 
+      (outDir:File, mibs:Seq[BuiltIn.Value], pkg:String, s:TaskStreams) =>
+        genMibs(outDir / "mibs", mibs, pkg, s)
     }
   )
 
 
-  def genMibs(dst:File, mibs:Seq[BuiltIn.Value], pkg:String) = { 
+  def genMibs(dst:File, mibs:Seq[BuiltIn.Value], pkg:String, s:TaskStreams) = { 
     val g = new Gen(pkg)
     val dir = pkg.split("\\.").foldLeft(dst){ case (d, next) => d / next }
+
+    s.log.info("Generating Scala source from "+mibs.size+" MIB"+(if(mibs.size == 1) "" else "s")+" to "+dst+"...")
 
     mibs map { mib => 
       val file = dir / (org.snmp4s.gen.Util.camel(mib.toString)+".scala")
