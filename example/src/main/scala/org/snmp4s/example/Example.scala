48806bc1db797ecd265e0b2cba1ca543a10d0b96
Joe Barnes <barnesjd@gmail.com>
2013-05-31 14:11:59 -0500

    Added a small sample application to the snmp4s-example project

---
+++ example/src/main/scala/org/snmp4s/example/Example.scala
@@ -0,0 +1,28 @@
+package org.snmp4s
+package example
+
+import Mib._
+import mib._
+import IfMib._
+import AgentppSimulationMib._
+import AgentppSimMode_enum._
+import Console.{println => p}
+
+object Example extends App {
+  val snmp = new Snmp(SnmpParams())
+
+  snmp.get(AgentppSimMode) match {
+    case Left(err)   => p("Get failed. "+err)
+    case Right(mode) => p(AgentppSimMode(0)+" == "+mode)
+  }
+  
+  snmp.set(AgentppSimMode to Config) match {
+    case Some(err) => p("Failed to set "+AgentppSimMode(0))
+    case _ => p("Set "+AgentppSimMode(0)+" to config")
+  }
+
+  snmp.get(AgentppSimMode) match {
+    case Left(err)   => p("Get failed. "+err)
+    case Right(mode) => p(AgentppSimMode(0)+" == "+mode)
+  }
+}
